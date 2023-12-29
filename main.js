a1f893c3a268dcb0e23e36442b1a885ab76dac6a
Pedro H. Bohrer <129559893+pedro-bohrer@users.noreply.github.com>
2023-12-29 20:40:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,41 @@
-// Const das listas
+// List
 const ListaDeSons = document.querySelectorAll('audio');
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-// Function do audio
-function tocaSom (idElementAudio) {
-    document.querySelector(idElementAudio).play(); 
+// Audio Function 
+
+function playAudio (audioSelector) {
+   const el = document.querySelector(audioSelector); 
+
+    if (el && el.localName === 'audio') {
+        el.play();      
+    }
+    else {
+        console.log("Ivalid Element or Selector!")
+    }
+    
 }
-
-
-//repeticao 
+//loop
 
 for (let cont = 0; cont < listaDeTeclas.length; cont++) {
 
     const idAudio = `#${ListaDeSons[cont].id}`
+    const tecla = listaDeTeclas[cont];
 
     listaDeTeclas[cont].onclick = function () {
-        tocaSom (idAudio)
-    };
+        playAudio (idAudio)
+    }
 
+//Action on key press
 
+    tecla.onkeydown = function (event) {
+
+        if (event.code === 'Space' || event.code === 'Enter'){
+
+        tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
